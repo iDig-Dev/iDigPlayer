392ce0b6b8c1344b3e92427d222087f0fc73c2a3
brainchildpro <james@brainchildproductions.com>
2012-07-01 14:59:34 -0700

    First Commit
    Horrible code everywhere...begining versioning

---
+++ src/in/idig/iDigPlayer/iDigPlayer.java
@@ -0,0 +1,56 @@
+package in.idig.iDigPlayer;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+
+public class iDigPlayer extends JavaPlugin{
+	
+	private Logger log = Logger.getLogger("Minecraft");
+	private CommandsHandler commandsHandler;
+
+	public CommandsHandler getCommandsHandler() {
+		return commandsHandler;
+	}
+	
+	public void onEnable(){
+		commandsHandler = new CommandsHandler(this);
+		getCommand("idig").setExecutor(commandsHandler);
+		
+		String PluginVersion = version();
+		this.logMessage("***************************");
+		this.logMessage("*   iDig Player Enabled    *");
+		this.logMessage("*   " + PluginVersion +"    *");
+		this.logMessage("***************************");
+		
+	}
+
+	public void onDisable(){
+		this.logMessage("***************************");
+		this.logMessage("*   iDigPlayer Disabled    *");
+		this.logMessage("***************************");
+	}
+
+	public void logMessage(String msg){
+		PluginDescriptionFile pdFile = this.getDescription();
+		this.log.info(pdFile.getName() + " " + pdFile.getVersion() + ": " + msg);
+	}
+
+	public String version(){
+		PluginDescriptionFile pdFile = this.getDescription();
+		return pdFile.getVersion();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (true)
+			sender.sendMessage(ChatColor.RED + "iDig CommandSender Linked in");
+		return true;
+	}
+
+}
